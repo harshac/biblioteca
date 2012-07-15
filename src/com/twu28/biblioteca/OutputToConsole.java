1d3705ed59d0654b23bbbc8fdce08f457e775e24
Harsha Chaudhary <harsha.chaudhary@gmail.com>
2012-07-15 18:19:57 +0530

    With TDD Approach 1. Requirement 1 covered

---
+++ src/com/twu28/biblioteca/OutputToConsole.java
@@ -0,0 +1,14 @@
+package com.twu28.biblioteca;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hp
+ * Date: 7/15/12
+ * Time: 5:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OutputToConsole {
+    public void printToConsole(String textToBePrinted) {
+        System.out.println(textToBePrinted);
+    }
+}
---
+++ test/com/twu28/biblioteca/TestLibrarySystem.java
@@ -0,0 +1,11 @@
+package com.twu28.biblioteca;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hp
+ * Date: 7/15/12
+ * Time: 5:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestLibrarySystem {
+}
