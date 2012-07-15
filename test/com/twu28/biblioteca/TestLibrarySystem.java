dc989e46fac4bbee89fe15e3ca64492c2aebc2cd
Harsha Chaudhary <harsha.chaudhary@gmail.com>
2012-07-15 18:24:04 +0530

    With TDD approach Requirement 1 covered

--- test/com/twu28/biblioteca/TestLibrarySystem.java
+++ test/com/twu28/biblioteca/TestLibrarySystem.java
@@ -1,5 +1,15 @@
 package com.twu28.biblioteca;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
 /**
  * Created with IntelliJ IDEA.
  * User: hp
@@ -8,4 +18,16 @@ package com.twu28.biblioteca;
  * To change this template use File | Settings | File Templates.
  */
 public class TestLibrarySystem {
+
+    @Test
+    public void showWelcomeMessageOnApplicationStart()
+    {
+        LibrarySystem librarySystem=new LibrarySystem();
+        TestOutput testOutput=new TestOutput();
+        testOutput.setUpOutputToPrintStream();
+        librarySystem.showWelcomeMessage();
+        assertEquals("Welcome !!" + testOutput.getLineSeparator(), testOutput.getOutputStream());
+    }
+
+
 }
--- test/com/twu28/biblioteca/TestOutput.java
+++ test/com/twu28/biblioteca/TestOutput.java
@@ -1,5 +1,14 @@
 package com.twu28.biblioteca;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created with IntelliJ IDEA.
  * User: hp
@@ -8,4 +17,37 @@ package com.twu28.biblioteca;
  * To change this template use File | Settings | File Templates.
  */
 public class TestOutput {
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpOutputToPrintStream()
+    {
+        System.setOut(new PrintStream(this.output));
+    }
+
+    @After
+    public  void tearDownOutputFromPrintStream()
+    {
+        System.setOut(null);
+    }
+
+    @Test
+    public void testIfMessageIsPrintedToConsole()
+    {
+        String lineSeparator=getLineSeparator();
+        OutputToConsole outputToConsole=new OutputToConsole();
+        outputToConsole.printToConsole("Welcome !!");
+        assertEquals("Welcome !!" + lineSeparator,output.toString());
+    }
+
+    public String getLineSeparator()
+    {
+        return System.getProperty("line.separator");
+    }
+
+    public String getOutputStream()
+    {
+        return this.output.toString();
+    }
+
 }
